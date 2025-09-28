e8f2f3bfd016df7f4fe3ab33a5a663b498d0fdfa
Ayodeji Akomolafe <akomolafeayodeji2007@gmail.com>
2025-09-28 14:09:06 +0100

    Aportfolio website i built from scratch

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+  const sidebar = document.querySelector('.sidebar,ul');
+  sidebar.style.display = 'flex'
+}
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar,ul");
+  sidebar.style.display = "none";
+}
